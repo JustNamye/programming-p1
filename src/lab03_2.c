e6b3af7de9c105e5d43ad6b1aa2225446fe2281d
JustNamye <justn@DESKTOP-9JFNDOV.localdomain>
2025-11-06 17:41:29 +0000

    added lab file

---
+++ src/lab03_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    
+    double a, b, c, d, e, x, E;
+    printf("Enter x: ");
+    scanf("%lf", &x);
+
+    a = cos(x);
+    b = sin(2*x);
+    c = exp(x);
+    d = tan(x);
+    e = log(x+9);
+
+    E = a/b + c*d/e;
+
+    printf("E(%.3f) = %.8f\n", x, E);
+
+    return 0;
+}
