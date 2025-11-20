af555e2dba6fd5bc6054eb708f78eb7f7f7179f0
JustNamye <justn@DESKTOP-9JFNDOV.localdomain>
2025-11-20 19:33:55 +0000

    added files

---
+++ lab5_1/series14_i.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+
+int main(void) {
+    // formula is x^n/2n
+    unsigned int x;
+    unsigned long long k;
+    unsigned long long S;
+
+    if (scanf("%d %llu", &x, &k) != 2) {
+        puts("Invalid input");
+        return 1;
+    }
+
+    for (unsigned long long n = 1; n <= k; n++)
+    {
+        __int128_t temp = (__int128_t) pow(x, n);
+        temp /= 2 * n;
+
+        if (temp < 0 || (__int128_t) S + temp > (__int128_t) ULLONG_MAX) {
+            puts("Overflow!");
+            return 2;
+        }
+        S+=(long long) temp;        
+    }
+    printf("%llu" "\n", S);
+    return 0;
+}
