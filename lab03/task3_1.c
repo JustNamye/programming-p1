a1f70e12a063aa6762ffb6e13c94a6a639ae8e48
JustNamye <justn@DESKTOP-9JFNDOV.localdomain>
2025-11-21 11:54:12 +0000

    removed unnecessary files, fixed codestyle

--- lab03/task3_1.c
+++ lab03/task3_1.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
+int main(void)
+{
     double x = 0.0;
-    double a, b, c, d, E;
+    double a; 
+    double b; 
+    double c; 
+    double d;
+    double E;
 
     printf("Input x: ");
     scanf("%lf", &x);
 
-    a = x*x - 6*x + 9;
+    a = x * x - 6 * x + 9;
     b = x - 3;
-    c = 3*x;
+    c = 3 * x;
     d = x + 3;
 
-    E=a/b+c/d;
+    E = a / b + c / d;
 
     printf("E(%.3f) = %.8f\n", x, E);
 
