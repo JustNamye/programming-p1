3258370d4acc44ad058a4c845e8e43d30d185fb4
JustNamye <justnamye@gmail.com>
2025-12-02 11:13:50 +0000

    Fixed codestyle

--- lab5_2/main.c
+++ lab5_2/main.c
@@ -3,17 +3,19 @@
 #include <stdbool.h>
 #include <math.h>
 
-int main(void) {
+int main(void)
+{
     double a = 0.0;
     double b = 2.0;
 
     int count;
 
-    if (scanf("%d", &count) != 1 || count <= 0) {
+    if (scanf("%d", &count) != 1 || count <= 0)
+    {
         puts("Invalid input");
         exit(EXIT_FAILURE);
     }
-    
+
     double step = (b - a) / (count - 1);
     double x = a;
 
@@ -21,20 +23,21 @@ int main(void) {
 
     printf("%10c %12c\n", 'x', 'd');
 
-    while (i<count)
+    while (i < count)
     {
         bool isDefined;
-        isDefined = cos(1 + x*x) != 0;
+        isDefined = cos(1 + x * x) != 0;
         if (isDefined)
         {
-            double tg = tan(sqrt(1+x*x));
-            double d = exp(-x*x/2)*tg;
+            double tg = tan(sqrt(1 + x * x));
+            double d = exp(-x * x / 2) * tg;
             printf("%10.5f, %12.5f\n", x, d);
         }
-        else {
+        else
+        {
             printf("%10.5f, %10s", x, "NaN");
         }
-        
+
         i++;
         x = a + i * step;
     }
